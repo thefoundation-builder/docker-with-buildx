43716fbbbe59ae00aabd02ebbe4c5f1150b27838
Benjamin Freitag <gitlab.com@b1nch0.us.to>
2023-03-20 12:36:30 +0000

    Update file finalize.sh

--- finalize.sh
+++ finalize.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-(which docker-squash 2>/dev/null |grep -q docker-squash ) || (which apk && ( apk add --no-cache py3-pip && pip3 install docker-squash ))
-(which docker-squash 2>/dev/null |grep -q docker-squash ) || ( which apt && ( apt update ; apt install -y python3-pip ;pip3 install docker-squash ))
+
+(which docker-squash 2>/dev/null |grep -q jq ) || ( which apk 2>/dev/null && ( apk add --no-cache jq git date  ))
+(which docker-squash 2>/dev/null |grep -q jq ) || ( which apt 2>/dev/null && ( apt update ; apt install -y procps jq git   ))
+
+( which apk && 
+apk add date psutils procps bash 
+#shadow 
+sed 's~/ash$~/bash~g' -i /etc/passwd
+)
+
+(which docker-squash 2>/dev/null |grep -q docker-squash ) || ( which apk 2>/dev/null && ( apk add --no-cache py3-pip && pip3 install docker-squash ))
+(which docker-squash 2>/dev/null |grep -q docker-squash ) || ( which apt 2>/dev/null && ( apt update ; apt install -y python3-pip ;pip3 install docker-squash ))
+
