ec65c2af1aa643a3c8be94b9ab83dafe046b9186
ı øŧħ³® ¢øµµ→ŧŧ€® <you@example.com>
2023-03-24 06:16:48 +0100

    2023-03-24-05.16

--- finalize.sh
+++ finalize.sh
@@ -4,11 +4,12 @@
 (which jq 2>/dev/null |grep -q jq ) || ( which apt 2>/dev/null && ( apt update ; apt install -y  jq git   ))
 
  which apk 2>/dev/null && (  
-apk add date psutils procps bash 
+apk add date psutils procps util-linux bash qemu-aarch64 qemu-x86_64 qemu-i386 qemu-arm
 #shadow 
 sed 's~/ash$~/bash~g' -i /etc/passwd
 )
 
 (which docker-squash 2>/dev/null |grep -q docker-squash ) || ( which apk 2>/dev/null && ( apk add --no-cache py3-pip && pip3 install docker-squash ))
 (which docker-squash 2>/dev/null |grep -q docker-squash ) || ( which apt 2>/dev/null && ( apt update ; apt install -y python3-pip ;pip3 install docker-squash ))
+
 
